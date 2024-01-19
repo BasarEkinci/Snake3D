20d3c34b9fbcb082f3dea39b4330ea18e99c8691
Başar Ekinci <basarekinci02@outlook.com>
2024-01-19 20:59:04 +0300

    Input Manager Script Added

---
+++ Snake3D/Assets/Runtime/Scripts/Managers/InputManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Snake3D.Runtime.Managers
+{
+    public class InputManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }    
+}
+
