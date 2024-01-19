620802c42cf40eceb6d2ebfdcaab08734ca35a81
Başar Ekinci <basarekinci02@outlook.com>
2024-01-19 21:12:29 +0300

    Input Manager Added

--- Snake3D/Assets/Runtime/Scripts/Managers/InputManager.cs
+++ Snake3D/Assets/Runtime/Scripts/Managers/InputManager.cs
@@ -4,7 +4,20 @@ namespace Snake3D.Runtime.Managers
 {
     public class InputManager : MonoBehaviour
     {
+        private PlayerInputs _playerInputs;
         
+        private void Awake()
+        {
+            _playerInputs = new PlayerInputs();
+            _playerInputs.Player.Enable();
+        }
+        
+        public Vector2 GetMovementInput()
+        {
+            Vector2 inputVector = _playerInputs.Player.Movement.ReadValue<Vector2>();
+            inputVector = inputVector.normalized;
+            return inputVector;
+        }
     }    
 }
 
