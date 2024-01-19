acad7c5f92f371d1c964ab379d519d8dafd07a78
Başar Ekinci <basarekinci02@outlook.com>
2024-01-19 21:05:29 +0300

    Singelton Extention Added

--- Snake3D/Assets/Runtime/Scripts/Managers/InputManager.cs
+++ Snake3D/Assets/Runtime/Scripts/Managers/InputManager.cs
@@ -4,17 +4,7 @@ namespace Snake3D.Runtime.Managers
 {
     public class InputManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
         
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }    
 }
 
