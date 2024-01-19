bdddde41b8f908fa4714041f9a3240f8ba356045
Başar Ekinci <basarekinci02@outlook.com>
2024-01-19 23:06:45 +0300

    Tail Controller Added

---
+++ Snake3D/Assets/Scripts/Runtime/Controllers/Player/PlayerTailController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Snake3D.Runtime.Controllers
+{
+    public class PlayerTailController : MonoBehaviour
+    {
+        [SerializeField] private GameObject tailPrefab;
+        [SerializeField] private int gapBetweenParts = 1;
+        
+        private List<GameObject> _bodyParts = new List<GameObject>();
+        private List<Vector3> _positionHistory = new List<Vector3>();
+        
+        public void Grow()
+        {
+            GameObject tail = Instantiate(tailPrefab, transform.position, Quaternion.identity);
+            _bodyParts.Add(tail);
+        }
+        public void SetTailPosition()
+        {
+            _positionHistory.Insert(0, transform.position);
+            int index = 0;
+
+            foreach (GameObject tail in _bodyParts)
+            {
+                Vector3 point = _positionHistory[Mathf.Min(index * gapBetweenParts, _positionHistory.Count - 1)];
+                tail.transform.position = point;
+                index++;
+            }
+        }
+    }    
+}
+
