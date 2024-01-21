7f2373e93161739251005a3e3c252e42f1c1ea77
Başar Ekinci <basarekinci02@outlook.com>
2024-01-21 20:24:24 +0300

    Sound Manager Added

--- Snake3D/Assets/Scripts/Runtime/Managers/PlayerManager.cs
+++ Snake3D/Assets/Scripts/Runtime/Managers/PlayerManager.cs
@@ -1,5 +1,6 @@
 using Runtime.Scripts.Controllers.Player;
 using Snake3D.Runtime.Controllers;
+using Snake3D.Runtime.Controllers.Player;
 using Snake3D.Runtime.Signals;
 using UnityEngine;
 
--- Snake3D/Assets/Scripts/Runtime/Managers/SoundManager.cs
+++ Snake3D/Assets/Scripts/Runtime/Managers/SoundManager.cs
@@ -1,12 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+using Snake3D.Runtime.Signals;
 using UnityEngine;
 
 namespace Snake3D.Runtime.Managers
 {
     public class SoundManager : MonoBehaviour
     {
-        
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private AudioClip deathSound;
+        [SerializeField] private AudioClip eatSound;
+
+        private void OnEnable()
+        {
+            PlayerSignals.Instance.OnCollectFood += OnCollectFood;
+            PlayerSignals.Instance.OnPlayerCrush += OnDeath;
+        }
+
+        private void OnDisable()
+        {
+            PlayerSignals.Instance.OnCollectFood -= OnCollectFood;
+            PlayerSignals.Instance.OnPlayerCrush -= OnDeath;
+        }
+
+        private void OnDeath()
+        {
+            audioSource.PlayOneShot(deathSound);
+        }
+
+        private void OnCollectFood()
+        {
+            audioSource.PlayOneShot(eatSound);
+        }
     }    
 }
 
